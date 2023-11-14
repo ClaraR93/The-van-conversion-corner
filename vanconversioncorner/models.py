ae66c36db6a173c1ca32b9f03431b28576d55795
ClaraR93 <>
2023-11-14 16:19:21 +0000

    Import and add timestamps to models.py

--- vanconversioncorner/models.py
+++ vanconversioncorner/models.py
@@ -1,4 +1,6 @@
 from vanconversioncorner import db
+# code used from python standard library, see README credits
+from datetime import datetime
 
 
 class Question(db.Model):
@@ -6,19 +8,21 @@ class Question(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question_title = db.Column(db.String(50), unique=True, nullable=False)
     question_body = db.Column(db.Text, nullable=False)
+    question_timestamp = db.Column(db.DateTime, default=datetime.utcnow) # code used from python standard library, see README credits
     comments = db.relationship(
         "Comment", backref="category", cascade="all, delete", lazy=True)
 
-    def__repre__(self):
-        return self
+    def __repre__(self):
+        return self.question_title, self.question_body
 
 
 class Comment(db.Model):
     # schema for Comment model
     id = db.Column(db.Integer, primary_key=True)
     comment_body = db.Column(db.Text, nullable=False)
+    comment_timestamp = db.Column(db.DateTime, default=datetime.utcnow) # code used from python standard library, see README credits
     question_id = db.Column(db.Integer, db.ForeignKey(
         "question.id", ondelete="CASCADE"))
 
-    def__repre__(self):
-        return self
+    def __repre__(self):
+        return self.comment_body
