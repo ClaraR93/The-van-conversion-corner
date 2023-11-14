721a4dd068ea8da70a4da32ae4bd09492671c21d
ClaraR93 <>
2023-11-14 20:42:54 +0000

    Add app.routes for post-your-questions page, and media page in routes.py

--- vanconversioncorner/routes.py
+++ vanconversioncorner/routes.py
@@ -6,3 +6,13 @@ from vanconversioncorner.models import Question, Comment
 @app.route("/")
 def home():
     return render_template("base.html")
+
+
+@app.route("/post-your-questions")
+def postYourQuestions():
+    return render_template("post-your-questions.html")
+
+
+@app.route("/media")
+def media():
+    return render_template("media.html")
