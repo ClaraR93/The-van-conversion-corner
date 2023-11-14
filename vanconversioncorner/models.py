b60946b30443ec3950c935de155d3c7199314812
ClaraR93 <>
2023-11-14 15:48:08 +0000

    Add columns for question_title, question_body, and comment_body in models.py

--- vanconversioncorner/models.py
+++ vanconversioncorner/models.py
@@ -4,6 +4,8 @@ from vanconversioncorner import db
 class Question(db.Model):
     # schema for Question model
     id = db.Column(db.Integer, primary_key=True)
+    question_title = db.Column(db.String(50), unique=True, nullable=False)
+    question_body = db.Column(db.Text, nullable=False)
 
     def__repre__(self):
         return self
@@ -12,6 +14,7 @@ class Question(db.Model):
 class Comment(db.Model):
     # schema for Comment model
     id = db.Column(db.Integer, primary_key=True)
+    comment_body = db.Column(db.Text, nullable=False)
 
     def__repre__(self):
         return self
